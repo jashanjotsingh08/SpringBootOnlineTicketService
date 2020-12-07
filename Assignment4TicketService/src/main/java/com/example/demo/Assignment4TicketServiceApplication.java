0f55b79c05e6ad1a31a3ea979ef2b959b9440e1a
Jashan <jashanjot.singh08@gmail.com>
2020-12-06 23:48:18 -0500

    customer fixed

--- Assignment4TicketService/src/main/java/com/example/demo/Assignment4TicketServiceApplication.java
+++ Assignment4TicketService/src/main/java/com/example/demo/Assignment4TicketServiceApplication.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class Assignment4TicketServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment4TicketServiceApplication.class, args);
 		System.out.println("Spring Boot JPA ticket-service started");
+		System.setProperty("spring.config.name", "ticket-service");
 	}
 
 }
